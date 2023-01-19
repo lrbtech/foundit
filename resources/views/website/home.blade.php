5da71b7046e3e6a4de6c4209fd39ce5de4e5f54d
prasanthlrb <prasanthats@gmail.com>
2023-01-20 00:42:31 +0530

    20-01-2023 v3

--- resources/views/website/home.blade.php
+++ resources/views/website/home.blade.php
@@ -3553,7 +3553,7 @@ $('#search').keydown( function(e) {
     var key = e.charCode ? e.charCode : e.keyCode ? e.keyCode : 0;
     if(key == 13) {
         //e.preventDefault();
-        SearchPost();
+        SearchPost1();
     }
 });
 </script>
